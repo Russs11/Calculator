56ff8096a7fc2262fbe526f594d3c1816ea11d57
Russs11 <russs.khalilov@gmail.com>
2022-07-26 14:16:15 +0300

    aded_js_operator_plus_26.07.2022

--- app/src/App.js
+++ app/src/App.js
@@ -4,12 +4,61 @@ import Container from './Componets/Container';
 import Btns from './Componets/Btns'
 import CalcScreen from './Componets/CalcScreen';
 
+
+
+
 function App() {
+  let operand = 0
+  let operand2 = 0
+  let operator = ""
+  function btnNumClickHandler(num) {
+    
+    if (operator === "") {
+    if (operand === 0) {
+      operand = num
+    } else {
+      operand = operand * 10 + num
+    }
+    }
+    if (operator !== "") {
+      if (operand2 === 0) {
+        operand2 = num
+      } else {
+        operand2 = operand2 * 10 + num
+      }
+    }
+    console.log("operand", operand);
+    console.log("operand2", operand2);
+    
+  
+  }
+  function operatorClickHandler(oper) {
+    operator = oper
+
+  }
+  function allClear() {
+    operand = 0
+    operand2 = 0
+    console.log("operand", operand);
+  }
+  function equallyClickHandler() {
+    if (operator === "+") {
+      let res = operand + operand2
+      console.log(res);
+      operator = ""
+      operand = 0
+      operand2 = 0
+    }
+  }
+
   return (
     <div className="App">
       <Container>
-        <CalcScreen>56566</CalcScreen>
-      <Btns />
+        <CalcScreen>0</CalcScreen>
+        <Btns btnNumClickHandler={btnNumClickHandler}
+          allClear={allClear}
+          operatorClickHandler={operatorClickHandler}
+          equallyClickHandler={equallyClickHandler} />
       </Container>
     </div>
   );
--- app/src/Componets/Btns.jsx
+++ app/src/Componets/Btns.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import "./btns.scss"
 
-const btns = () => {
+const btns = ({ btnNumClickHandler, allClear, operatorClickHandler, equallyClickHandler }) => {
     return (
         <div className="btns">
-            <div className="btn ac bg-grey">AC</div>
+            <div className="btn ac bg-grey" onClick={allClear}>AC</div>
             <div className="btn plus-minus bg-grey">+/-</div>
             <div className="btn percent bg-grey">%</div>
             <div className="btn devide">/</div>
 
-            <div className="btn seven ">7</div>
-            <div className="btn eight">8</div>
-            <div className="btn nine">9</div>
+            <div className="btn seven " onClick={() => { btnNumClickHandler(7) }}>7</div>
+            <div className="btn eight" onClick={() => { btnNumClickHandler(8) }}>8</div>
+            <div className="btn nine" onClick={() => { btnNumClickHandler(9) }}>9</div>
             <div className="btn multiply bg-orange">X</div>
 
-            <div className="btn four">4</div>
-            <div className="btn five">5</div>
-            <div className="btn six">6</div>
+            <div className="btn four" onClick={() => { btnNumClickHandler(4) }}>4</div>
+            <div className="btn five" onClick={() => { btnNumClickHandler(5) }}>5</div>
+            <div className="btn six" onClick={() => { btnNumClickHandler(6) }}>6</div>
             <div className="btn minus bg-orange">-</div>
 
-            <div className="btn one">1</div>
-            <div className="btn two">2</div>
-            <div className="btn three">3</div>
-            <div className="btn plus bg-orange">+</div>
+            <div className="btn one" onClick={() => {btnNumClickHandler(1)}}>1</div>
+            <div className="btn two" onClick={() => { btnNumClickHandler(2) }}>2</div>
+            <div className="btn three" onClick={() => { btnNumClickHandler(3) }}>3</div>
+            <div className="btn plus bg-orange" onClick={() => { operatorClickHandler ("+")}}>+</div>
 
-            <div className="btn zero">0</div>
+            <div className="btn zero" onClick={() => { btnNumClickHandler(0) }}>0</div>
             <div className="btn dot">.</div>
-            <div className="btn equal bg-orange">=</div>
+            <div className="btn equal bg-orange" onClick={equallyClickHandler}>=</div>
         </div>
     );
 };
