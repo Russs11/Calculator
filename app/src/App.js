01f99b5ce70faa304b242f3b33ad69e56572dd24
Russs11 <russs.khalilov@gmail.com>
2022-07-27 17:42:19 +0300

    aded_js_operator_plus_27.07.2022

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Container from './Componets/Container';
 import Btns from './Componets/Btns'
 import CalcScreen from './Componets/CalcScreen';
-
+import {useState} from 'react'
 
 
 
@@ -11,51 +11,91 @@ function App() {
   let operand = 0
   let operand2 = 0
   let operator = ""
+  let numAfterDot = ""
+  const [operandState, setOperatorState] = useState(0)
   function btnNumClickHandler(num) {
 
     if (operator === "") {
       if (operand === 0) {
         operand = num
-      } else {
+      }
+      else {
         operand = operand * 10 + num
       }
     }
+    if (numAfterDot === ".") {
+      operand = operand / 10
+    }
+
+
     if (operator !== "") {
       if (operand2 === 0) {
         operand2 = num
-      } else {
+      }
+      else {
         operand2 = operand2 * 10 + num
       }
+    }
+    if (numAfterDot === ".") {
+      operand2 = operand2 / 10
+
     }
     console.log("operand", operand);
     console.log("operand2", operand2);
 
-
   }
+  function btnDotClickHandler(sign) {
+    if (sign === ".") {
+      numAfterDot = sign
+
+    }
+  }
+
   function operatorClickHandler(oper) {
     operator = oper
-
+    console.log(operator);
   }
   function allClear() {
     operand = 0
     operand2 = 0
+    operator = ""
+    numAfterDot = ""
     console.log("operand", operand);
+    console.log("operand2", operand2);
+    console.log("operator", operator);
   }
   function equallyClickHandler() {
     if (operator === "+") {
       operand = operand + operand2
+      console.log("res", operand);
+      operator = ""
+      numAfterDot = ""
+      operand2 = 0
+    }
+    if (operator === "-") {
+      operand = operand - operand2
+      console.log("res", operand);
       operator = ""
       operand2 = 0
-
-      console.log("res", operand);
     }
+
     if (operator === "x") {
       operand = operand * operand2
       console.log("res", operand);
       operator = ""
       operand2 = 0
-      
-   }
+    }
+
+    if (operator === "/") {
+      if (operand2 === 0) {
+        console.log("Ошибка");
+        allClear()
+      }
+      operand = operand / operand2
+      console.log("res", operand);
+      operator = ""
+      operand2 = 0
+    }
 
   }
 
@@ -66,7 +106,9 @@ function App() {
         <Btns btnNumClickHandler={btnNumClickHandler}
           allClear={allClear}
           operatorClickHandler={operatorClickHandler}
-          equallyClickHandler={equallyClickHandler} />
+          equallyClickHandler={equallyClickHandler}
+          btnDotClickHandler={btnDotClickHandler}
+        />
       </Container>
     </div>
   );
--- app/src/Componets/Btns.jsx
+++ app/src/Componets/Btns.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import "./btns.scss"
 
-const btns = ({ btnNumClickHandler, allClear, operatorClickHandler, equallyClickHandler }) => {
+const btns = ({ btnNumClickHandler, allClear, operatorClickHandler, equallyClickHandler, btnDotClickHandler }) => {
     return (
         <div className="btns">
             <div className="btn ac bg-grey" onClick={allClear}>AC</div>
             <div className="btn plus-minus bg-grey">+/-</div>
             <div className="btn percent bg-grey">%</div>
-            <div className="btn devide">/</div>
+            <div className="btn devide" onClick={() => { operatorClickHandler("/") }}>/</div>
 
             <div className="btn seven " onClick={() => { btnNumClickHandler(7) }}>7</div>
             <div className="btn eight" onClick={() => { btnNumClickHandler(8) }}>8</div>
@@ -17,7 +17,7 @@ const btns = ({ btnNumClickHandler, allClear, operatorClickHandler, equallyClick
             <div className="btn four" onClick={() => { btnNumClickHandler(4) }}>4</div>
             <div className="btn five" onClick={() => { btnNumClickHandler(5) }}>5</div>
             <div className="btn six" onClick={() => { btnNumClickHandler(6) }}>6</div>
-            <div className="btn minus bg-orange">-</div>
+            <div className="btn minus bg-orange" onClick={() => { operatorClickHandler("-") }}>-</div>
 
             <div className="btn one" onClick={() => {btnNumClickHandler(1)}}>1</div>
             <div className="btn two" onClick={() => { btnNumClickHandler(2) }}>2</div>
@@ -25,7 +25,7 @@ const btns = ({ btnNumClickHandler, allClear, operatorClickHandler, equallyClick
             <div className="btn plus bg-orange" onClick={() => { operatorClickHandler ("+")}}>+</div>
 
             <div className="btn zero" onClick={() => { btnNumClickHandler(0) }}>0</div>
-            <div className="btn dot">.</div>
+            <div className="btn dot" onClick={() => { btnDotClickHandler(".") }}>.</div>
             <div className="btn equal bg-orange" onClick={equallyClickHandler}>=</div>
         </div>
     );
