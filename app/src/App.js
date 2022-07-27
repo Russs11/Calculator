d725c34b7e53eccd504e6b414026ae27d71110ac
Russs <russs.khalilov@gmail.com>
2022-07-28 00:12:34 +0300

    added_hooks_28.07.2022

--- app/src/App.js
+++ app/src/App.js
@@ -3,98 +3,120 @@ import './App.css';
 import Container from './Componets/Container';
 import Btns from './Componets/Btns'
 import CalcScreen from './Componets/CalcScreen';
-import {useState} from 'react'
+import { useState } from 'react'
 
 
 
 function App() {
-  let operand = 0
-  let operand2 = 0
-  let operator = ""
-  let numAfterDot = ""
-  const [operandState, setOperatorState] = useState(0)
-  function btnNumClickHandler(num) {
+  // let operand = 0
+  // let operand2 = 0
+  // let operator = ""
+  // let numAfterDot = ""
+  const [operand1, setOperand1] = useState(0)
+  const [operand2, setOperand2] = useState("")
+  const [operator, setOperator] = useState("")
+  const [result, setResult] = useState(0)
+  const [numAfterDot, setNumAfterDot] = useState("")
 
-    if (operator === "") {
-      if (operand === 0) {
-        operand = num
-      }
-      else {
-        operand = operand * 10 + num
-      }
-    }
-    if (numAfterDot === ".") {
-      operand = operand / 10
-    }
-
-
-    if (operator !== "") {
-      if (operand2 === 0) {
-        operand2 = num
-      }
-      else {
-        operand2 = operand2 * 10 + num
-      }
-    }
-    if (numAfterDot === ".") {
-      operand2 = operand2 / 10
-
-    }
-    console.log("operand", operand);
-    console.log("operand2", operand2);
-
+  function allClear() {
+    setOperand1(0);
+    setOperand2("");
+    setOperator("");
+    setResult("");
+    setNumAfterDot("");
   }
+  function btnNumClickHandler(num) {
+    // if (operator === "") {
+    //   if (operand === 0) {
+    //     operand = num
+    //   }
+    //   else {
+    //     operand = operand * 10 + num
+    //   }
+    // }
+    
+    // if (operator !== "") {
+      //   if (operand2 === 0) {
+        //     operand2 = num
+        //   }
+        //   else {
+          //     operand2 = operand2 * 10 + num
+          //   }
+          // }
+          // if (numAfterDot === ".") {
+            //   operand2 = operand2 / 10
+            
+            // }
+            // console.log("operand", operand);
+            // console.log("operand2", operand2);
+            
+            if (operator === "") {
+              setOperand1(operand1 + num);
+            } else {
+              setOperand2(operand2 + num);
+            }
+            if (numAfterDot === ".") {
+              setOperand1((operand1 / 10) + num)
+            }
+          }
+
+
+
+
   function btnDotClickHandler(sign) {
     if (sign === ".") {
-      numAfterDot = sign
-
+      setNumAfterDot(sign)
     }
   }
 
   function operatorClickHandler(oper) {
-    operator = oper
-    console.log(operator);
+    setOperator(oper)
   }
-  function allClear() {
-    operand = 0
-    operand2 = 0
-    operator = ""
-    numAfterDot = ""
-    console.log("operand", operand);
-    console.log("operand2", operand2);
-    console.log("operator", operator);
-  }
+
+  // function equallyClickHandler() {
+  //   switch (operator) {
+  //     case "+":
+  //       setResult(Number(operand1) + Number(operand2));
+  //       break;
+  //     case "-":
+  //       setResult(Number(operand1) - Number(operand2));
+  //       break;
+  //     case "x":
+  //       setResult(Number(operand1) * Number(operand2));
+  //       break;
+  //     case "/":
+  //       setResult(Number(operand1) / Number(operand2));
+  //       break;
+  //   }
+  // }
   function equallyClickHandler() {
     if (operator === "+") {
-      operand = operand + operand2
-      console.log("res", operand);
-      operator = ""
-      numAfterDot = ""
-      operand2 = 0
+
+      setResult(Number(operand1) + Number(operand2))
+      // numAfterDot = ""
+        setOperator("")
+        setOperand2(0)
     }
     if (operator === "-") {
-      operand = operand - operand2
-      console.log("res", operand);
-      operator = ""
-      operand2 = 0
+      setResult(Number(operand1) - Number(operand2))
+      setOperator("")
+      setOperand2(0)
     }
 
     if (operator === "x") {
-      operand = operand * operand2
-      console.log("res", operand);
-      operator = ""
-      operand2 = 0
+      setResult(Number(operand1) * Number(operand2))
+      setOperator("")
+      setOperand2(0)
     }
 
     if (operator === "/") {
-      if (operand2 === 0) {
-        console.log("Ошибка");
+      if (operand2 === 0 || operand1 === Infinity) {
         allClear()
+        setResult("Ошибка");
       }
-      operand = operand / operand2
-      console.log("res", operand);
-      operator = ""
-      operand2 = 0
+      setResult(Number(operand1) / Number(operand2))
+      setOperator("")
+      setOperand2(0)
     }
 
   }
@@ -102,7 +124,10 @@ function App() {
   return (
     <div className="App">
       <Container>
-        <CalcScreen>0</CalcScreen>
+        <CalcScreen>
+          <div className="previous-operand">{operator ? operand1 + operator : ""}</div>
+          <div className="current-operand">{result ? result : (!operator ? operand1 : operand2)}</div>
+        </CalcScreen>
         <Btns btnNumClickHandler={btnNumClickHandler}
           allClear={allClear}
           operatorClickHandler={operatorClickHandler}
